a53724f58fbbe657edef9d86d45d05c6c5e7e677
honeeee <knn92@hanmail.net>
2020-04-22 20:10:00 +0900

    8393

---
+++ src/BaekJoon/Joon8393.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Joon8393 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int sum=0;
+		for(int i=1; i<=n; i++){
+		    sum+=i;
+		}
+		System.out.print(sum);
+
+	}
+
+}
