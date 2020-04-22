a32b2e14f4f3660f2791aa5bd368d0e504d0afed
honeeee <knn92@hanmail.net>
2020-04-23 02:43:00 +0900

    10950

---
+++ src/BaekJoon/Joon10950.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Joon10950 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//System.out.println("¸î°³?");
+		int t = sc.nextInt();
+		
+		for(int i=1; i<=t; i++) {
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+
+	}
+
+}
