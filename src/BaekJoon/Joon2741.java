32b0d9e3d9ef8298e0e067251d554dfdaacae506
honeeee <knn92@hanmail.net>
2020-04-24 15:34:00 +0900

    2741

---
+++ src/BaekJoon/Joon2741.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Joon2741 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			
+			System.out.println(i);
+		}
+
+	}
+
+}
