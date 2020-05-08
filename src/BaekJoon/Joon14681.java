77674f0703ffa3a1672c5673e2c3cd7e511b585a
honeeee <knn92@hanmail.net>
2020-05-08 15:24:00 +0900

    14681,15552

---
+++ src/BaekJoon/Joon14681.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Joon14681 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if(x>=1 && y>=1)
+			System.out.println("1");
+		
+		else if(x<=-1 && y>=1)
+			System.out.println("2");
+		
+		else if(x<=-1 && y<=-1)
+			System.out.println("3");
+		
+		else if(x>=1 && y<=-1)
+			System.out.println("4");
+		
+	}
+
+}
